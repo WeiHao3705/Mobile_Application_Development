4dd2cfdeb0b726d21fb5318cb7d12d1df7a5e369
liewcb <143771761+liewcb@users.noreply.github.com>
2026-03-05 03:24:41 +0800

    test

--- lib/main.dart
+++ lib/main.dart
@@ -92,6 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _isLoading = true;
       _errorMessage = '';
     });
+    //test
 
     try {
       print('🔌 Getting Supabase client...');
